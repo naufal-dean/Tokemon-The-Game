c7c8176bfe07ac4cae53b7429635a93421310bd9
naufal-dean <naufal.dean@gmail.com>
2019-11-05 17:29:52 +0700

    Bug fix, add specialAttack.

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 :- dynamic(gameStarted/1, at/3, enemy/1, myToke/1, playerName/1, moves/1).
-:- dynamic(battleStarted/1, encounter/1, activeToke/1, enemyToke/8).
+:- dynamic(battleStarted/1, encounter/1, activeToke/2, enemyToke/9).
 
 :- include(initgame).
 :- include(util).
